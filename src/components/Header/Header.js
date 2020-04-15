d9542e60399ed65bcd71df3bbd50d2e540d6220a
Agatha Rudko <agatharudko@gmail.com>
2020-04-15 19:50:38 +0300

    routing partly

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,34 @@
 import React from "react";
 import {cn} from '@bem-react/classname'
 import './Header.scss';
-import {Link} from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 const headerCN = cn('header');
 export  const Header = props =>{
-    return (<div className={headerCN('container')}>
+    return (<Router>
+        <div className={headerCN('container')}>
 <h1 className={headerCN('label')}>Vita Media Planer</h1>
-        {/*<p className={headerCN('login')}>Логин: Агата_Рудько</p>*/}
-        {/*<button className={headerCN('btn')}>Выйти</button>*/}
-    </div>)
+            <Switch>
+                <Route path={"/main"}>
+                    <div className={headerCN('info')}>
+                    <p className={headerCN('login')}>Логин: Агата_Рудько</p>
+                    <button className={headerCN('btn')}>Выйти</button>
+                    </div>
+                </Route>
+                <Route path={"/ro"}>
+                    <div className={headerCN('info')}>
+        <p className={headerCN('login')}>Логин: Агата_Рудько</p>
+        <button className={headerCN('btn')}>Выйти</button>
+                    </div>
+                </Route>
+            </Switch>
+    </div>
+        </Router>
+    )
 
 };
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {cn} from '@bem-react/classname'
 import './Main.scss';
+import { Link } from 'react-router-dom';
 
 const mainCN = cn('main');
 export  const Main = () =>{
@@ -9,7 +10,9 @@ export  const Main = () =>{
         <div className={mainCN('btn-container')}>
         <button className={mainCN('btn')}>Сформировать список РО</button>
         <button className={mainCN('btn')}>Создать коммерческое предложение</button>
-        <button className={mainCN('btn')}>Создать рекламный объект</button>
+           <Link to={"/ro"}>
+        <button className={mainCN('btn')} >Создать рекламный объект</button>
+           </Link>
         </div>
     </div>)
 
