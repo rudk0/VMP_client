7d4f962f0389a05a04e5ad683afde9cb49c231c1
Agatha Rudko <agatharudko@gmail.com>
2020-05-04 23:27:52 +0300

    kp adding

--- src/api/KPAPI.js
+++ src/api/KPAPI.js
@@ -2,8 +2,13 @@ import axios from "axios"
 import {API_URL} from "./baseAPI";
 
 export const KPApi = {
-  formEstimate: async (ids)=>{
+  formEstimate: async (ids) => {
     return axios.post(`${API_URL}cp_form_estimate`, ids)
+  },
+  preview: async (estimates, b1_price) => {
+    return axios.post(`${API_URL}cp_count/${b1_price}`, estimates)
+  },
+  formKp: async (data) => {
+    return axios({url: `${API_URL}cp`, method: 'post',responseType: 'blob', data})
   }
-
 }
--- src/components/common/TextInput/TextInput.js
+++ src/components/common/TextInput/TextInput.js
@@ -3,8 +3,7 @@ import './TextInput.scss'
 import {cn} from "@bem-react/classname";
 const textInputCn = cn('text-input')
 export const TextInput = props =>{
-  const { prop, name, type, label, onChange, onFocusOut}=props
-  console.log(prop);
+  const {  name, type, label, onChange, onFocusOut}=props
   return (
     <label className={textInputCn('label')}> {label}
       <input type={type} onBlur={onFocusOut ? e=>{e.persist(); onFocusOut(e)}: null} className={textInputCn('input')}  name={name}  onChange={onChange ? e=>onChange(e) : null} {...props}/>
