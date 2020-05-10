9ba6132eec79b20130dbccb69c16981005d29c1c
Agatha Rudko <agatharudko@gmail.com>
2020-05-10 15:29:57 +0300

    menu pr

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,23 +6,8 @@ import { Link } from 'react-router-dom';
 
 const mainCN = cn('main');
 export const Main = () =>{
-    console.log(localStorage.role);
-    if (localStorage.role !== 'ROLE_ADMIN'){
-    return (<div className={mainCN('container')}>
-        <h2 className={mainCN('label')}>Главная страница</h2>
-        <div className={mainCN('btn-container')}>
-            <Link to={"/ro"}>
-        <button className={mainCN('btn')}>Сформировать список РО</button>
-            </Link>
-            <Link to={"/kpbuilder"}>
-        <button className={mainCN('btn')}>Создать коммерческое предложение</button>
-            </Link>
-            <Link to={"/robuilder"}>
-        <button className={mainCN('btn')} >Создать рекламный объект</button>
-            </Link>
-        </div></div>)
-            }else
-            {return(<div className={mainCN('container')}>
+
+    return(<div className={mainCN('container')}>
                 <h2 className={mainCN('label')}>Главная страница</h2>
                 <div className={mainCN('btn-container')}>
                     <Link to={"/ro"}>
@@ -35,11 +20,11 @@ export const Main = () =>{
                         <button className={mainCN('btn')} >Создать рекламный объект</button>
                     </Link>
             <Link to={'/users'}>
-                <button className={mainCN('btn')} >Управление пользователями</button>
+                {localStorage.role==='ROLE_ADMIN' && <button className={mainCN('btn')} >Управление пользователями</button>}
             </Link>
             <Link to={'/db'}>
-                <button className={mainCN('btn')} >Управление Базой данных</button>
+                {localStorage.role==='ROLE_ADMIN' && <button className={mainCN('btn')} >Управление Базой данных</button>}
             </Link>
         </div>
-    </div>)}};
+    </div>)};
 
